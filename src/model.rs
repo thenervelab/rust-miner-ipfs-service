431c3233e6a581b255dd297aa24007affe394be3
lat-murmeldjur <lat.murmeldjur@gmail.com>
2025-09-23 20:57:48 +0300

    fix optional strings in json read

--- src/model.rs
+++ src/model.rs
@@ -7,12 +7,12 @@ pub struct FileInfo {
     pub cid_v2: String,
     pub created_at: u64,
     pub file_hash: Vec<u8>,
-    pub file_id: String,
+    pub file_id: Option<String>,
     pub file_size_in_bytes: u64,
-    pub miner_node_id: String,
-    pub original_name: String,
-    pub owner: String,
-    pub selected_validator: String,
+    pub miner_node_id: Option<String>,
+    pub original_name: Option<String>,
+    pub owner: Option<String>,
+    pub selected_validator: Option<String>,
     pub size_bytes: u64,
 }
 
